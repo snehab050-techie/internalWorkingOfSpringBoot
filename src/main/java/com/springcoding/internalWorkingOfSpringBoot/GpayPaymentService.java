2a6bb0ace1bd06d2b70b3152e23da67f2b0cff62
snehab050-techie <snehab050@gmail.com>
2026-05-04 19:31:22 +0530

    Committing changes for conditionally creating beans with configuration

--- src/main/java/com/springcoding/internalWorkingOfSpringBoot/GpayPaymentService.java
+++ src/main/java/com/springcoding/internalWorkingOfSpringBoot/GpayPaymentService.java
@@ -1,7 +1,9 @@
 package com.springcoding.internalWorkingOfSpringBoot;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 @Component //to make this class a spring bean and to be managed by spring container
+@ConditionalOnProperty(name = "payment.provider", havingValue = "gpay") // to conditionally create this bean based on the property value
 public class GpayPaymentService implements PaymentService {
 
     @Override
--- src/main/java/com/springcoding/internalWorkingOfSpringBoot/PhonePayPaymentService.java
+++ src/main/java/com/springcoding/internalWorkingOfSpringBoot/PhonePayPaymentService.java
@@ -1,8 +1,10 @@
 package com.springcoding.internalWorkingOfSpringBoot;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
-// @Component - to make this class a spring bean and to be managed by spring container
+@Component // to make this class a spring bean and to be managed by spring container
+@ConditionalOnProperty(name = "payment.provider", havingValue = "phonepay") // to conditionally create this bean based on the property value
 public class PhonePayPaymentService implements PaymentService {
 
     @Override
